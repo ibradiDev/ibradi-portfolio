4f654f4759db99e7de37f8df12e9930f7bba83bf
ibradiDev <ibradi.dev@gmail.com>
2024-12-15 19:44:05 +0000

    Created pages for contact, resume, services, and work, along with a header and mobile navigation. Included animations and statistics display components. (NB: There is some "lorem ipsum" texts for development)

---
+++ components/WorkSliderBtns.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import {useSwiper} from "swiper/swiper-react";
+
+const WorkSliderBtns = (containerStyles, btnStyles, iconStyles) => {
+    const swiper=useSwiper()
+    return (
+        <div className={containerStyles}>
+
+        </div>
+    )
+}
+export default WorkSliderBtns;
